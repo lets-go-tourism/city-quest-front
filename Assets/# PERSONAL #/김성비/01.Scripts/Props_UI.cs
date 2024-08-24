44166d357e6103a1a268355ad174603b730a413b
JoanneKael <135231453+JoanneKael@users.noreply.github.com>
2024-08-24 20:09:26 +0900

    0824
    - 탐험 팝업창
    - 메인뷰

---
+++ Assets/# PERSONAL #/김성비/01.Scripts/Props_UI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Props_UI : MonoBehaviour
+{
+    // º¯¼ö
+    public Canvas canvasS;
+    public Canvas canvasM;
+    public Transform prop;
+    public ScrollView scrollView;
+    
+    public static Props_UI instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // UI On/Off ¼³Á¤
+    public void PropsUISetting(bool isOpen)
+    {
+        // Äµ¹ö½º
+        canvasM.enabled = isOpen;
+        canvasS.enabled = isOpen;
+        // 3D ¸ðµ¨¸µ
+        prop.gameObject.SetActive(isOpen);
+        
+    }
+}
