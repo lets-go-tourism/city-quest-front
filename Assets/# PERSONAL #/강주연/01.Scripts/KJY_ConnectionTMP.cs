826759edee9c0b3fa2ce0b677d67e4711d71af7b
juyeon0514 <sk651726@gmail.com>
2024-08-25 20:48:06 +0900

    KJY_GPS
    -GPS기능추가

--- Assets/# PERSONAL #/강주연/01.Scripts/KJY_ConnectionTMP.cs
+++ Assets/# PERSONAL #/강주연/01.Scripts/KJY_ConnectionTMP.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
